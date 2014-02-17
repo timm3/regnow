71a2567159fe29843ac9714c3ba750104d6e3bab
Perry Huang <perry.huang@gmail.com>
2014-02-17 04:29:22 -0600

    Configure more app options.

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require 'sinatra'
 configure do
   set :bind, '0.0.0.0'
   set :public_folder, Proc.new { File.join(root, "static") }
+  set :server, "puma"
+  set :views
+  set :port, 80
+  set :environment, :development
 end
 
 get '/hi' do
