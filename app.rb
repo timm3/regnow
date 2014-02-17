5459b71a5f02bde978a47251e2321b7b807372b0
Perry Huang <perry.huang@gmail.com>
2014-02-17 04:43:56 -0600

    Fix views bug

--- app.rb
+++ app.rb
@@ -4,11 +4,10 @@ configure do
   set :bind, '0.0.0.0'
   set :public_folder, Proc.new { File.join(root, "static") }
   set :server, "puma"
-  set :views
-  set :port, 80
+  set :port, "80"
   set :environment, :development
 end
 
-get '/hi' do
-  "Hello World!"
+get '/' do
+  "Hello world"
 end
