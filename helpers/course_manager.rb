20dd43282540c313bf63e86ba001c2c232374b0e
Perry Huang <perry.huang@gmail.com>
2014-02-24 02:53:18 -0600

    Add courses collections reset capability.

--- helpers/course_manager.rb
+++ helpers/course_manager.rb
@@ -3,4 +3,10 @@ module CourseManager
     course = Course.create(:name => name, :crn => crn, :code => code, :instructor => instructor)
     course.save
   end
+
+  def CourseManager.reset()
+    Course.delete_all
+    
+    # TODO: populate courses collection here...
+  end
 end
