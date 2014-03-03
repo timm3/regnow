f1ed78d4a2bd46bbfe460fec280056871fdab089
Perry Huang <perry.huang@gmail.com>
2014-03-02 18:32:37 -0600

    Add more attributes to course model.

--- models/course.rb
+++ models/course.rb
@@ -1,8 +1,13 @@
 class Course
   include MongoMapper::Document
 
-  key :name,        String
-  key :crn,         Integer
-  key :code,        String
-  key :instructor,  String
+  key :name,          String
+  key :crn,           Integer
+  key :code,          String
+  key :instructor,    String
+  key :open_slots,    Integer
+  key :total_slots,   Integer
+  key :section,       String
+  key :subject,       String
+  key :type,          String
 end
