2dc264f88851889d5d224cab30cec3ab183a47d4
Perry Huang <perry.huang@gmail.com>
2014-03-03 18:37:57 -0600

    Add mechanize dep.

--- helpers/course_manager.rb
+++ helpers/course_manager.rb
@@ -1,7 +1,13 @@
+require 'mechanize'
+
 module CourseManager
   def CourseManager.add_course(name, crn, code, instructor)
     course = Course.create(:name => name, :crn => crn, :code => code, :instructor => instructor)
     course.save
+  end
+
+  def CourseManager.get_spots(crn)
+
   end
 
   def CourseManager.reset()
