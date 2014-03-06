958e65e7499cedb102ce9f57c282f79513cbfae5
Perry Huang <perry.huang@gmail.com>
2014-03-06 00:06:20 -0600

    Add text message notification.

--- helpers/notification_manager.rb
+++ helpers/notification_manager.rb
@@ -3,8 +3,17 @@ require 'mail'
 
 module NotificationManager
 
-  def NotificationManager.send_text_notification(user, crn)
-    # TODO
+  def NotificationManager.send_text_notification(number=false, crn=false)
+    if !number || crn
+      return false
+    end
+    client = Twilio::REST::Client.new($twilio_account_sid, $twilio_auth_token)
+    client.account.message.create(
+      :from => '+14158304303',
+      :to => "+1" + number,
+      :body => "RegNow! Your course CRN #{crn} now has a spot open!"
+    )
+    return true
   end
 
   def NotificationManager.send_email_notification(user, crn)
