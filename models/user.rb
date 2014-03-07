7dafc60e99118c9e681b6a119f38c4bab662ede4
Perry Huang <huang185@illinois.edu>
2014-03-07 05:53:16 -0600

    Update user model to include notifications.

--- models/user.rb
+++ models/user.rb
@@ -1,9 +1,12 @@
 class User
   include MongoMapper::Document
 
-  key :name,                  String
-  key :netid,                 String
-  key :password,              String
-  key :join_date,             Date
-  key :notification_only,     Boolean
+  key :name,          String
+  key :netid,         String
+  key :password,      String
+  key :join_date,     Date
+  key :text_only,     Boolean
+  key :email_only,    Boolean
+  key :number,        String
+  key :crns,          Array
 end
