b966868791fdb2591ed04ab3e370b869b59d23b3
Kurtis Houdek <houdekk@gmail.com>
2014-03-19 18:06:10 -0500

    added registration_manager to be used for main thread

---
+++ helpers/registration_manager.rb
@@ -0,0 +1,9 @@
+module RegistrationManager
+
+  #one iteration of updating all classes available and registering
+  # users from queues if spots are open
+  def update_classes_and_queues()
+
+  end
+
+end
