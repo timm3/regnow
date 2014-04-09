264f71b8d96812cc89456c7943b70d8e82727fe7
Perry Huang <perry.huang@gmail.com>
2014-04-09 06:25:08 -0500

    Fix to use random parameterized unit tests.

--- tests/database_tests.rb
+++ tests/database_tests.rb
@@ -4,25 +4,36 @@ require 'rack/test'
 
 class RegNowDatabaseTest < Test::Unit::TestCase
 
-  def test_add_course()
-    DatabaseManager.add_course("test course", ["-1337"], "TST", "Test Professor")
-    assert_equal true, DatabaseManager.check_crn("-1337")
+  def initialize
+    @crn = rand(-1000..-1).to_s
+    @netid = SecureRandom.hex
+    @course = SecureRandom.hex
+    @subject = SecureRandom.hex
+    @professor = SecureRandom.hex
+    @password = SecureRandom.hex
+    @netid_password = SecureRandom.hex
+    @date = DateTime.now
   end
 
-  def test_delete_course()
-    DatabaseManager.remove_course("-1337")
-    assert_equal false, DatabaseManager.check_crn("-1337")
+  def test_add_course
+    DatabaseManager.add_course(@course, [@crn], @subject, @professor)
+    assert_equal true, DatabaseManager.check_crn(@crn)
   end
 
-  def test_add_user()
-    DatabaseManager.add_user( "Test T. Name", "student2", "test_password", "test_netid_password",
-                              DateTime.now, false, false, "", [] )
+  def test_delete_course
+    DatabaseManager.remove_course(@crn)
+    assert_equal false, DatabaseManager.check_crn(@crn)
+  end
+
+  def test_add_user
+    DatabaseManager.add_user(SecureRandom.hex, @netid, @password,
+                              @netid_password, @date, false, false, "", [] )
     assert_equal true, DatabaseManager.check_netid("student2")
   end
 
-  def test_delete_user()
-    DatabaseManager.remove_user("student2")
-    assert_equal false, DatabaseManager.check_netid("student2")
+  def test_delete_user
+    DatabaseManager.remove_user(@netid)
+    assert_equal false, DatabaseManager.check_netid(netid)
   end
 
 end
--- tests/queue_tests.rb
+++ tests/queue_tests.rb
@@ -4,36 +4,41 @@ require 'rack/test'
 
 class RegNowUserQueueTest < Test::Unit::TestCase
 
+  def initialize
+    @crn = rand(-1000..-1).to_s
+    @netid = SecureRandom.hex
+  end
+
   def setup
-    QueueManager.create_queue("-1337")
+    QueueManager.create_queue(@crn)
   end
 
   def teardown
-    UserQueue.destroy_all(:crn => "-1337")
+    UserQueue.destroy_all(:crn => @crn)
   end
 
   def test_create_queue
     #queue = UserQueue.create(:crn => "-1337", :netids => [])
-    queue = UserQueue.first(:crn => "-1337")
+    queue = UserQueue.first(:crn => crn)
     assert_not_nil queue
   end
 
   def test_add_user
-    QueueManager.add_user("-1337", "student")
-    queue = QueueManager.get_queue("-1337")
-    assert_equal true, queue[:netids].include?("student")
+    QueueManager.add_user(@crn, @netid)
+    queue = QueueManager.get_queue(@crn)
+    assert_equal true, queue[:netids].include?(@netid)
   end
 
   def test_remove_user
-    QueueManager.add_user("-1337", "student")
-    QueueManager.remove_user("-1337", "student")
-    queue = QueueManager.get_queue("-1337")
-    assert_equal false, queue[:netids].include?("student")
+    QueueManager.add_user(@crn, @netid)
+    QueueManager.remove_user(@crn, @netid)
+    queue = QueueManager.get_queue(@crn)
+    assert_equal false, queue[:netids].include?(@netid)
   end
 
   def test_next_user
-    QueueManager.add_user("-1337", "student")
-    assert_equal "student", QueueManager.get_next_user("-1337")
+    QueueManager.add_user(@crn, @netid)
+    assert_equal "student", QueueManager.get_next_user(@crn)
   end
 
 end
