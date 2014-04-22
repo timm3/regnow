93035c28a8fda411f776f482f0e2bebd8c5c06b3
Kurtis Houdek <houdekk@gmail.com>
2014-04-21 21:43:46 -0500

    add courses working, tests passing

--- tests/courses_tests.rb
+++ tests/courses_tests.rb
@@ -10,27 +10,27 @@ class RegNowCoursesTest < Test::Unit::TestCase
   end
 
   def setup
-    @test_bot = CourseManager.new
+    @test_bot = CourseManager.new('student1','test')
   end
 
   def test_get_open_spots
     open_spots = @test_bot.get_open_spots("52738")
-    assert_equal open_spots, 3
+    assert_equal 3, open_spots
   end
 
   def test_get_total_spots
     total_spots = @test_bot.get_total_spots("52738")
-    assert_equal total_spots, 10
+    assert_equal 10, total_spots
   end
 
   def test_register_invalid_course
-    result = @test_bot.register_course("1")
-    assert_equal result, false
+    result = @test_bot.register_crn_list(["11111"])
+    assert_equal false, result
   end
 
   def test_register_valid_course
-    result = @test_bot.register_course("52738")
-    assert_equal result, true
+    result = @test_bot.register_crn_list(["12344","12345"])
+    assert_equal true, result
   end
 
   def test_course_crns
